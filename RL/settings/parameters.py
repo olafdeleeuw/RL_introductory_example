0459f9989804565ec8573570682c1214483ad8fa
OlafdeL <Olaf@dataworkz.nl>
2019-10-29 16:56:55 +0100

    updated parameters

--- RL/settings/parameters.py
+++ RL/settings/parameters.py
@@ -10,13 +10,18 @@ class RLParameters:
     """
 
     def __init__(self):
-        self.reward_length = 1
-        self.reward_mof = 10
-        self.reward_connected_houses = 25
+        self.reward_length = -1
+        self.reward_mof = -10
         self.episodes = 1e4
+        self.cables = 2
+        self.learning_rate = 0.5
+        self.epsilon = 0.1
 
     def set_number_of_episodes(self, episodes):
         self.episodes = episodes
+
+    def set_number_of_cables(self, cables):
+        self.cables = cables
 
     def update_reward_length(self, new_reward_length):
         self.reward_length = new_reward_length
@@ -26,3 +31,9 @@ class RLParameters:
 
     def update_reward_houses(self, new_reward_houses):
         self.reward_connected_houses = new_reward_houses
+
+    def update_learning_rate(self, new_learning_rate):
+        self.learning_rate = new_learning_rate
+
+    def update_epsilon(self, new_epsilon):
+        self.epsilon = new_epsilon
